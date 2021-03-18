d83c6138996cf6ce03bd2aaad0736e420277cece
AmadouDiallo4 <diallobis2011@gmail.com>
2021-03-18 19:01:28 +0100

    Add spring boot project

---
+++ src/test/java/com/cicd/cicdapplicationspringbootjavaapp/CiCdApplicationSpringbootJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdapplicationspringbootjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationSpringbootJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
